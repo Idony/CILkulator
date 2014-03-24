1c824c71a2f34280e426933ad2f63a802065fd7b
Artem <Hezhay@mail.ru>
2014-03-24 12:46:41 +0700

    adder SingleCalculators

---
+++ Calc/Calc/SingleCalculators/ISingleOperation.cs
@@ -0,0 +1,7 @@
+﻿namespace Calc.SingleCalculators
+{
+    public interface ISingleOperation
+    {
+        double Calculation(double firstArgument); 
+    }
+}
---
+++ Calc/Calc/SingleCalculators/SingleOperationFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc.SingleCalculators
+{
+    public static class SingleOperationFactory
+    {
+        public static ISingleOperation Create(string calculatorName)
+        {
+            switch (calculatorName)
+            {
+                case "":
+                   // return new Adder();
+                default:
+                    throw new ArgumentException("ПИШИ ПРАВИЛЬНО УВАСЯ!!", "calculatorName");
+
+            }
+        }
+    }
+}
