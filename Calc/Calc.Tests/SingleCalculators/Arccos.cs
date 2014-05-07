69e47a37ccc753f4dc5cbc48db8dfbd0cad44293
maxeemov <maxeemov@gmail.com>
2014-05-07 18:33:10 +0700

    correct arccostest

--- Calc/Calc.Tests/SingleCalculators/Arccos.cs
+++ Calc/Calc.Tests/SingleCalculators/Arccos.cs
@@ -16,14 +16,14 @@ namespace Calc.Tests.SingleCalculators
         {
             ISingleOperation calc = SingleOperationFactory.Create("Arccos");
             double result = calc.Calculation(0.5);
-            Assert.AreEqual(result, 1.0471975511966);
+            Assert.AreEqual(result, Math.Acos(0.5));
         }
         [Test]
         public void ArccosTest2()
         {
             ISingleOperation calc = SingleOperationFactory.Create("Arccos");
             double result = calc.Calculation(0.3);
-            Assert.AreEqual(result, 1.2661036727795);
+            Assert.AreEqual(result, Math.Acos(0.3));
         }
     }
 }
