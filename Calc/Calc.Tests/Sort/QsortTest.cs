fb7f6d9a52636a91c66889bf9368420cbbb25e94
Idony <Hezhay@mail.ru>
2014-05-19 12:44:44 +0700

    Add tests for sort

---
+++ Calc/Calc.Tests/Sort/QsortTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Calc.Sorts;
+using NUnit.Framework;
+
+namespace Calc.Tests.Sort
+{
+    [TestFixture]
+    public class QsortTest
+    {
+        [Test]
+        public void QsortTest1()
+        {
+            ISort sort = SortOperationFactory.Create("qsort");
+            List<int> list = new List<int>() { 9, 2, 4, 3, 1 };
+            List<int> result = sort.Sort(list);
+            Assert.AreEqual(result, new List<int>() { 1, 2, 3, 4, 9 });
+        }
+        [Test]
+        public void QsortTest2()
+        {
+            ISort sort = SortOperationFactory.Create("qsort");
+            List<int> list = new List<int>() { 9, 2, 4, 3, 1,0 };
+            List<int> result = sort.Sort(list);
+            Assert.AreEqual(result, new List<int>() {0, 1, 2, 3, 4, 9 });
+        }
+        
+    }
+}
